3501b27a819972809a72577a8fc45fa43c47b382
ykomissarov <lilipp1q@yandex.ru>
2019-01-25 15:41:08 +0300

    default config

---
+++ mock/default_config.go
@@ -0,0 +1,49 @@
+package mock
+
+import (
+	"github.com/integration-system/isp-lib/database"
+	"github.com/integration-system/isp-lib/structure"
+)
+
+const (
+	DefaultConfigServiceHost = "isp-config-service"
+	DefaultConfigServicePort = "9001"
+)
+
+type ConfigServiceLocalConfiguration struct {
+	Database         database.DBConfiguration
+	GrpcOuterAddress structure.AddressConfiguration
+	ModuleName       string
+	WS               struct {
+		Rest structure.AddressConfiguration
+		Grpc structure.AddressConfiguration
+	}
+}
+
+func DefaultConfigServiceConfiguration() ConfigServiceLocalConfiguration {
+	return ConfigServiceLocalConfiguration{
+		Database: database.DBConfiguration{
+			Address:      "isp-pg",
+			Port:         "5432",
+			Schema:       "config-service",
+			Database:     "isp-test",
+			Username:     "isp-test",
+			CreateSchema: true,
+		},
+		GrpcOuterAddress: structure.AddressConfiguration{
+			IP:   DefaultConfigServiceHost,
+			Port: "9002",
+		},
+		ModuleName: "config",
+		WS: struct {
+			Rest structure.AddressConfiguration
+			Grpc structure.AddressConfiguration
+		}{Rest: structure.AddressConfiguration{
+			IP:   "0.0.0.0",
+			Port: DefaultConfigServicePort,
+		}, Grpc: structure.AddressConfiguration{
+			IP:   "0.0.0.0",
+			Port: "9002",
+		}},
+	}
+}
