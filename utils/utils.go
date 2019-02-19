bb5b7b1f6668923284fa7f1d65274855dd290129
d1slike <merser1q@gmail.com>
2019-02-19 13:57:23 +0300

    ref

--- utils/utils.go
+++ utils/utils.go
@@ -8,32 +8,55 @@ import (
 )
 
 var (
-	ErrDeadlineExceeded = errors.New("connect deadline exceeded")
+	ErrDeadlineExceeded = errors.New("deadline exceeded")
 )
 
-func AwaitConnection(dialer func() (interface{}, error), timeout time.Duration) (interface{}, error) {
-	connChan := make(chan interface{})
-	ctx, _ := context.WithTimeout(context.Background(), timeout)
+type TimeoutRetryer struct {
+	f                   func() (interface{}, error)
+	DeadlineTimeout     time.Duration
+	AttemptTimeout      time.Duration
+	AttemptErrorHanlder func(err error)
+}
 
-	reconnect := int32(1)
+func (r *TimeoutRetryer) Do() (interface{}, error) {
+	ch := make(chan interface{})
+	ctx, _ := context.WithTimeout(context.Background(), r.DeadlineTimeout)
+
+	retrying := int32(1)
 	go func() {
-		for atomic.LoadInt32(&reconnect) == 1 {
-			c, err := dialer()
+		for atomic.LoadInt32(&retrying) == 1 {
+			c, err := r.f()
 			if err != nil {
-				time.Sleep(500 * time.Millisecond)
+				if r.AttemptErrorHanlder != nil {
+					r.AttemptErrorHanlder(err)
+				}
+				time.Sleep(r.AttemptTimeout)
 				continue
 			} else {
-				connChan <- c
+				ch <- c
 				return
 			}
 		}
 	}()
 
 	select {
-	case conn := <-connChan:
-		return conn, nil
+	case value := <-ch:
+		return value, nil
 	case <-ctx.Done():
-		atomic.StoreInt32(&reconnect, 0)
+		atomic.StoreInt32(&retrying, 0)
 		return nil, ErrDeadlineExceeded
+	}
+}
+
+func AwaitConnection(dialer func() (interface{}, error), timeout time.Duration) (interface{}, error) {
+	return NewRetryer(dialer, timeout).Do()
+}
+
+func NewRetryer(f func() (interface{}, error), deadlineTimeout time.Duration) *TimeoutRetryer {
+	return &TimeoutRetryer{
+		f:                   f,
+		DeadlineTimeout:     deadlineTimeout,
+		AttemptTimeout:      500 * time.Millisecond,
+		AttemptErrorHanlder: nil,
 	}
 }
